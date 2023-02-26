17161ea8ddda1e039cff69e5183e4c865d5e03ef
Roman <122228334+rdseveryukhin@users.noreply.github.com>
2023-02-26 22:07:09 +0400

    Добавьте файлы проекта.Create README.md

---
+++ ClassSharedPtr/ConsoleApplication10/ConsoleApplication10.cpp
@@ -0,0 +1,98 @@
+﻿#include <iostream>
+
+struct Expression {
+	Expression (const char* str) : str(str) {}
+	const char* str;
+};
+struct Number;
+struct BinaryOperation;
+
+struct SharedPtr
+{
+	explicit SharedPtr(Expression *ptr = 0) {
+		this->ptr_ = ptr;
+		this->count = new size_t(0);
+	}
+
+	~SharedPtr() {
+		if ((*count) > 0) {
+			(*count)--;
+		}
+		else {
+			delete ptr_;
+			delete count;
+		}
+	}
+
+	SharedPtr(const SharedPtr &other) {
+		this->ptr_ = other.ptr_;
+		this->count = other.count;
+		(*count)++;
+	}
+
+	SharedPtr& operator=(const SharedPtr &other) {
+		if (this != &other) {
+			if ((*count) > 0) {
+				(*count)--;
+			}
+			else {
+				delete ptr_;
+				delete count;
+			}
+			this->ptr_ = other.ptr_;
+			this->count = other.count;
+			(*count)++;
+		}
+		return *this;
+	}
+
+	Expression* get() const {
+		return ptr_;
+	}
+
+	void reset(Expression *ptr = 0) {
+		if ((*count) > 0) {
+			(*count)--;
+		}
+		else {
+			delete ptr_;
+			delete count;
+		}
+		ptr_ = ptr;
+		count = new size_t(0);
+	}
+
+	Expression& operator*() const {
+		return *ptr_;
+	}
+
+	Expression* operator->() const {
+		return ptr_;
+	}
+private:
+	Expression* ptr_;
+	size_t* count;
+};
+
+int main() {
+
+	SharedPtr p1;
+	SharedPtr p2(new Expression("expr1"));
+	SharedPtr p3(new Expression("expr2"));
+	SharedPtr p4(p2);
+	SharedPtr p5;
+	p5 = p2;
+	p5 = p4;
+	p1 = p5;
+	p3.reset(NULL);
+	p3 = p5;
+	p5.reset(NULL);
+	SharedPtr p6;
+	SharedPtr p7;
+	p7 = p7;
+	p7.reset(NULL);
+	p7.reset(new Expression("expr3"));
+	SharedPtr p8(new Expression("expr4"));
+	p8.reset(NULL);
+	p1 = p1;
+}
